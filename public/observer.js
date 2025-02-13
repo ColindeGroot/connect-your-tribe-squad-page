57a12f280112452cf25e21f8e1b88fa8ee120437
Colin de Groot <colindegroot2004@gmail.com>
2025-02-13 15:58:35 +0100

    observer gemaakt en gestyled close #2

---
+++ public/observer.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("slide-in");
+            }
+        });
+    }, { threshold: 0.35 }); // zichtbaar van element dat nodig is om uit te voeren heid 1 = 100%, 0.1 = 10%
+
+    document.querySelectorAll(".person-list li").forEach(item => {
+        observer.observe(item);
+    });
+});
